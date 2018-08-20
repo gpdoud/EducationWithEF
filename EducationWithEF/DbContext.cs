3959d0d04e9a57bce33a758fff8b5069593fedaa
Greg Doud <gpdoud@gmail.com>
2018-08-20 06:47:56 -0400

    final commit

---
+++ EducationWithEF/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationWithEF {
+	public class MyDbContext : DbContext {
+
+		public MyDbContext() : base("Name=EducationWithEFConnection") {
+		}
+
+		//public virtual DbSet<Student> Students { get; set; }
+	}
+}
---
+++ EducationWithEF/Migrations/201709181423528_Added student.cs
@@ -0,0 +1,16 @@
+namespace EducationWithEF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Addedstudent : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
